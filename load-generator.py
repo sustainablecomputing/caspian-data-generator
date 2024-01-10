9b678ab7224249367308b175abed155c3212693c
Tayebeh Bahreini <tayebehbahreini@Tayebehs-MacBook-Pro.local>
2024-01-10 08:01:15 -0600

    Update

--- load-generator.py
+++ load-generator.py
@@ -16,6 +16,7 @@ kube_path='~/.kube/config'
 n=1             # number of jobs
 TT = 1          # number of time slots to gegerate load
 period_length=60
+Overal_T=48
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
 # for a given job id,  if there is no job in workspace with that name, it creates greenjob object, namespace, and a deployment inside that namespace 
@@ -25,7 +26,7 @@ def create_job(job_id,run_time,duration,deadline,cpu,gpu):
         path=os.path.expanduser('~')+"/caspian-demo"
         os.chdir(path+"/script")
         os.system('./create-aw.sh aw'+str(job_id)+"\t"+str(run_time)+"\t"+str(duration)+"\t"+str(deadline)+"\t"+str(cpu)+"\t"+str(gpu))
-        subprocess.run('kubectl config use-context kind-hub ',shell=True)
+        subprocess.run('kubectl config use-context k3d-hub ',shell=True)
     
         subprocess.run(' kubectl apply -f'+path+'/temp/aw'+str(job_id)+'.yaml',shell=True)
         
@@ -34,7 +35,7 @@ def create_job(job_id,run_time,duration,deadline,cpu,gpu):
 #create jobs for a time horizon T
 def create_all_jobs():
     numpy.random.seed(1)
-    subprocess.run('kubectl config use-context kind-hub ',shell=True)
+    subprocess.run('kubectl config use-context k3d-hub ',shell=True)
                
     if len(sys.argv)>2:
         T=int(sys.argv[1])
@@ -59,18 +60,61 @@ def create_all_jobs():
                 l=period_length*(1+numpy.random.randint(0,5))
                 d = (now+ datetime.timedelta(0,2*l)).strftime('%Y-%m-%dT%H:%M:%SZ')
                 print(d)
-                cpu=0#float(1+numpy.random.randint(0,2))/10
-                gpu=int(1+numpy.random.randint(0,8))
+                cpu=0#float(1+numpy.random.randint(0,2))/15
+                gpu=int(3+numpy.random.randint(0,8))
                 create_job(job_id,l,l,d,cpu,gpu)
                 job_id=job_id+1
             subprocess.run('KUBECONFIG='+kube_path+' kubectl get appwrappers',shell=True)
+            if t==1:
+                subprocess.run('kubectl config use-context k3d-cluster1 ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster1-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent -- > ~/caspian-demo/util/util-spoke1.txt ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster1-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >  ~/caspian-demo/util/util-spoke1-w1.txt ',shell=True)
+               # subprocess.run('kubectl describe node k3d-cluster1-agent-1 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >  ~/caspian-demo/util/util-spoke1-w2.txt ',shell=True)
+                subprocess.run('kubectl config use-context k3d-cluster2 ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster2-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >  ~/caspian-demo/util/util-spoke2.txt ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster2-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >  ~/caspian-demo/util/util-spoke2-w1.txt ',shell=True)
+                #subprocess.run('kubectl describe node k3d-cluster2-agent-1 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >  ~/caspian-demo/util/util-spoke2-w2.txt ',shell=True)
+                subprocess.run('kubectl config use-context k3d-cluster3 ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster3-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >  ~/caspian-demo/util/util-spoke3.txt ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster3-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >  ~/caspian-demo/util/util-spoke3-w1.txt ',shell=True)
+               
             if t>1:
+                    
+                    subprocess.run('kubectl config use-context k3d-cluster1 ',shell=True)
+                    subprocess.run('kubectl describe node k3d-cluster1-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke1.txt ',shell=True)
+                    subprocess.run('kubectl describe node k3d-cluster1-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke1-w1.txt ',shell=True)
+                   # subprocess.run('kubectl describe node k3d-cluster1-agent-1 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke1-w2.txt ',shell=True)
+                    subprocess.run('kubectl config use-context k3d-cluster2 ',shell=True)
+                    subprocess.run('kubectl describe node k3d-cluster2-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke2.txt ',shell=True)
+                    subprocess.run('kubectl describe node k3d-cluster2-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke2-w1.txt ',shell=True)
+                    #subprocess.run('kubectl describe node k3d-cluster2-agent-1 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke2-w2.txt ',shell=True)
+                    subprocess.run('kubectl config use-context k3d-cluster3 ',shell=True)
+                    subprocess.run('kubectl describe node k3d-cluster3-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke3.txt ',shell=True)
+                    subprocess.run('kubectl describe node k3d-cluster3-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke3-w1.txt ',shell=True)
+                   
                     subprocess.run('cp -f ~/caspian-demo/CI/'+str(t-1)+'/CA-ON.csv ~/caspian/monitoring/data/CA-ON.csv ',shell=True)
-                    subprocess.run('cp -f ~/caspian-demo/CI/'+str(t-1)+'/US-TEX-ERCO.csv ~/caspian/monitoring/data/US-TEX-ERCO.csv ',shell=True)
+                    subprocess.run('cp -f ~/caspian-demo/CI/'+str(t-1)+'/JP-KN.csv ~/caspian/monitoring/data/JP-KN.csv ',shell=True)
+                    subprocess.run('cp -f ~/caspian-demo/CI/'+str(t-1)+'/DE.csv ~/caspian/monitoring/data/DE.csv ',shell=True)
             time.sleep(period_length)
         
- 
 
+        for t in range(T,Overal_T):  
+                subprocess.run('kubectl config use-context k3d-cluster1 ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster1-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke1.txt ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster1-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke1-w1.txt ',shell=True)
+               # subprocess.run('kubectl describe node k3d-cluster1-agent-1 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke1-w2.txt ',shell=True)
+                subprocess.run('kubectl config use-context k3d-cluster2 ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster2-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke2.txt ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster2-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent -- >>  ~/caspian-demo/util/util-spoke2-w1.txt ',shell=True)
+               # subprocess.run('kubectl describe node k3d-cluster2-agent-1 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke2-w2.txt ',shell=True)
+                subprocess.run('kubectl config use-context k3d-cluster3 ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster3-server-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent  -- >>  ~/caspian-demo/util/util-spoke3.txt ',shell=True)
+                subprocess.run('kubectl describe node k3d-cluster3-agent-0 | grep Allocated -A 15 | grep -ve Event -ve Allocated -ve percent -- >>  ~/caspian-demo/util/util-spoke3-w1.txt ',shell=True)
+                   
+                subprocess.run('cp -f ~/caspian-demo/CI/'+str((t-1)%24)+'/CA-ON.csv ~/caspian/monitoring/data/CA-ON.csv ',shell=True)
+                subprocess.run('cp -f ~/caspian-demo/CI/'+str((t-1)%24)+'/JP-KN.csv ~/caspian/monitoring/data/JP-KN.csv ',shell=True)
+                subprocess.run('cp -f ~/caspian-demo/CI/'+str((t-1)%24)+'/DE.csv ~/caspian/monitoring/data/DE.csv ',shell=True)
+                time.sleep(period_length)
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
 
