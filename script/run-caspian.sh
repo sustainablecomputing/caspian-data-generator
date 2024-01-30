ddbcad1b953b15ce8559f7e4e18eda929e44dfad
Tayebeh Bahreini <tayebehbahreini@Tayebehs-MacBook-Pro.local>
2024-01-30 12:09:54 -0600

    Update

--- script/run-caspian.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Change to the directory containing the Python script
-cd ~/caspian
-go run ./cmd/main.go --kube-context=k3d-hub --optimizer=sustainable //optimizer good be sustaible or QoS
