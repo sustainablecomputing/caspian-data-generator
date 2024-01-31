296e3ed1ec1756a81084322d5f77482152624fa2
Tayebeh Bahreini <tayebehbahreini@Tayebehs-MacBook-Pro.local>
2024-01-31 16:40:12 -0600

    Add optimizationMode

--- script/run-caspian.sh
+++ script/run-caspian.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+############################################################
+#
+# run-caspian
+#
+# Usage: <shellName> [<optimizationMode>]
+#
+#         where <optimizationMode> could be sustainable or qos
+#         (default sustainable)
+############################################################
 . demo-setenv.sh
+optimizationMode="sustainable"
+if [[ $1 && $1 -gt 0 ]]
+then
+	optimizationMode=$1
+fi
+
 cd $CASPIAN_DIR
 context=${CONTEXT_PREFIX}${HUB}
-go run ./cmd/main.go --kube-context=$context --optimizer=sustainable #optimizer could work in two-modes: sustainable or QoS
+go run ./cmd/main.go --kube-context=$context --optimizer=$optimizationMode #optimizer could work in two-modes: sustainable or QoS
