03b0452a79246da143aec54a8ff64a1179befdce
Tayebeh Bahreini <tayebehbahreini@Tayebehs-MacBook-Pro.local>
2024-01-31 16:34:53 -0600

    Update steps

--- script/run-mcad.sh
+++ script/run-mcad.sh
@@ -9,12 +9,12 @@
 #         (default 1)
 #
 # Steps:
-# (1) Install MCAD in hub 
-# (2) Run MCAD Dispatcher in hub
+#  (1) Install MCAD in hub 
+#  (2) Run MCAD Dispatcher in hub
 #  For each spoke cluster i in {1,2,...,numClusters}
 #  (3) Install MCAD 
 #  (4) Run MCAD Runner in spoke
-#  (5) run syncer for syning between hub and the spoke
+#  (5) Run syncer for syning between hub and the spoke
 ############################################################
 
 
@@ -35,8 +35,8 @@ make install
 #Step (2):
 go run ./cmd/main.go --kube-context=$hub_context --mode=dispatcher --metrics-bind-address=127.0.0.1:8080 --health-probe-bind-address=127.0.0.1:8081 &
 
-#MCAD Runner gets as input the geo-location and power profile of the cluster and update the clusterinfo accordingly
-# A sample values for 10 clusters are provided below.
+#MCAD Runner gets as input the geo-location and power profile of the clusters and update 
+# the clusterinfo accordingly. A sample input values for 10 clusters are provided below.
 zones=("CA-ON" "DE" "JP-KN" "GB" "AU-TAS" "FR" "US-TEX-ERCO" "US-NY-NYIS" "US-CAL-CISO" "IN-EA") #geo-location of clusters
 idealPower=(100 100 100 100 100 100 100 100 100 100)  #average ideal power of each cluster
 peakPower=(300 300 300 330 300 300 300 300 300 300)    #average peak power of each cluster
