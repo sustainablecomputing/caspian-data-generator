c9f2535ee35ae89b4146dbca2b7b2ad555d23e04
Tayebeh Bahreini <tayebehbahreini@Tayebehs-MacBook-Pro.local>
2024-02-12 02:10:37 -0600

    Modify

--- code/monitoring.py
+++ code/monitoring.py
@@ -16,22 +16,22 @@ class Monitoring(object):
     
     api_client=None
     api_instance=None
-    frequency=10                # frequency of updating the plot (every 10 seconds)
+
+    frequency=60                # frequency of updating the plot (every 60 seconds)
     
     clusterInfos=[]             # clusterInfo objects in the hub
-    Slots=36                     # number of time slots. Each slot is 6*10 seconds
-    perSlot=6                      #number of bars  per slot to display
-    T=36*6                      #number of bars in the plot (to visualize system over 36*6*10 seconds)
-    
+    T=72                    # number of time slots. Each slot is 6*60 seconds
+    perSlot=6                      #number of bars  per slot to display  
     M=3                         # number of available clusterinfo (default value is 3)
  
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
 # create client to talk to the hub
-    def __init__(self,hub_context):         
+    def __init__(self,hub_context,frequency):         
         self.api_client=kube_config.new_client_from_config(config_file=self.kube_config,context=self.contxt)
         self.api_instance = kubernetes.client.CustomObjectsApi(self.api_client)
         self.contxt=hub_context
+        self.frequency=frequency
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
 # get clusterInfo objects living in the hub and update the list of clusterInfos
@@ -48,13 +48,14 @@ class Monitoring(object):
             clusterinfos=api_response.get('items')           
             
             for ci in clusterinfos:
-                print(ci['metadata']['name']) 
+               
                 if 'usage' in ci['status'] and 'nvidia.com/gpu' in ci['status']['usage']:
                     tmp = { 'name':ci['metadata']['name'],'gpu':ci['status']['usage']['nvidia.com/gpu'],'gpu-cap':ci['status']['capacity']['nvidia.com/gpu'],'carbon': ci['spec']['carbon']}
                 else:
                    tmp = { 'name':ci['metadata']['name'],'gpu':'0','gpu-cap':ci['status']['capacity']['nvidia.com/gpu'],'carbon': ci['spec']['carbon']}
                 
-                self.clusterInfos.append(tmp)                                 
+                self.clusterInfos.append(tmp)    
+                print(tmp['gpu'])                     
         except ApiException as e:
             print("Exception when calling CustomObjectsApi->get_cluster_custom_object: %s\n" % e)
 
@@ -96,9 +97,11 @@ class Monitoring(object):
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
 hub_context="k3d-hub"
-if len(sys.argv)>1:
+frequency=60
+if len(sys.argv)>2:
      hub_context=str(sys.argv[0])
-m=Monitoring(hub_context)
+     frequency=int(sys.argv[1])
+m=Monitoring(hub_context,frequency)
 T=m.T
 M=m.M
 
@@ -109,9 +112,6 @@ em_rate=[0]*T
 slow_down=[1,1,1,1.2,1.2,1.4,1.4,1.3,1.4,1.5,1.4,1.5,1.6,1.5,1.5,1.6,1.6,1.7,1.7,1.5,1.7,1.6,1.7,1.6,1.5,1.6,1.5,1.5,1.6,1.6,1.7,1.7,1.5,1.5,1.6,1.5]
 lateness=[1,1,1,1.2,1.2,1,1.1,1.1,1.3,1.3,1.2,1.2,1.3,1.3,1.1,1.1,1.2,1.7,1.2,1.5,1.7,1.1,1.4,1.6,1.5,1.1,1.1,1.3,1.2,1.3,1.2,1.4,1.1,1.5,1.2,1.2]
 
-zones=[ 'CA-ON','US-NY','GB']
-dframe=pd.read_csv('./data/CI.csv', usecols= ['timestamp', 'CA-ON','US-NY','GB'])
-carbon=[]
 carbonIntensity=[[0 for i in range(T)] for j in range(M)]
 
 carbon_metric=[0]*T
@@ -133,26 +133,27 @@ for j in range(M):
 for j in range(3):
  ax2.append(fig2.add_subplot(1,3,1+j))
 
-def animate(i):
+def animate_cluster(i):
+    m.get_clusterInfos()
     for j in range(M):
-        for t in range(i,m.perSlot*24+i):
-            if t<T:
-                carbonIntensity[j][t]=dframe.iloc[int(t/m.perSlot)][zones[j]]
-       
+        for t in range(0,24):
+            if i+t<T:
+                carbonIntensity[j][t+i]=int(m.clusterInfos[0]['carbon'][t])#dframe.iloc[int(t/m.perSlot)][zones[j]]
     if i>0:
         carbon_metric[i]=carbon_metric[i-1]
         
-    m.get_clusterInfos()
+   
     for j in range(M):
         utilization[j][i]=float(m.clusterInfos[0]['gpu'])/float(m.clusterInfos[0]['gpu-cap'])#test: (3-j)*0.15#
         
-    xs=range(0,int(T))
+     
+    xs=range(0,T)
 
     for j in range(M):  
         ax[j].clear()
-        ax[j].axis([0, T, 0, 300.1])
-        ax[j].set_xticks(ticks=range(0,int(T),int(2*T/m.Slots)),labels=range(1,m.Slots+1,2),fontsize=5) #every 6 bars add label of slot
-        ax[j].set_yticks(ticks=range(0,400,50),labels=range(0,400,50),fontsize=5)
+        ax[j].axis([0, T, 0, 700.1])
+        ax[j].set_xticks(ticks=range(5,T+1,5),labels=range(5,T+1,5),fontsize=5) #every 6 bars add label of slot
+        ax[j].set_yticks(ticks=range(100,700,100),labels=range(100,700,100),fontsize=5)
         ax[j].set_ylabel('Carbon Intensity', fontsize = 8)
         ax[j].set_xlabel('Time slots', fontsize = 8)
         ax[j].step(xs, carbonIntensity[j],color =colors[j])
@@ -161,43 +162,37 @@ def animate(i):
         ax[j+M].clear()
         ax[j+M].axis([0, T, 0, 1.1])
         ax[j+M].set_yticks(ticks=range(0,2),labels=range(0,2,1),fontsize=5)
-        ax[j+M].set_xticks(ticks=range(1,int(T),int(2*T/m.Slots)),labels=range(1,m.Slots+1,2),fontsize=5) #every 6 bars add label of slot 
+        ax[j+M].set_xticks(ticks=range(5,T+1,5),labels=range(5,T+1,5),fontsize=5) #every 6 bars add label of slot
         ax[j+M].set_ylabel('GPU utilization', fontsize = 8)
         ax[j+M].set_xlabel('Time slots', fontsize = 8)
         ax[j+M].bar(xs, utilization[j],color =colors[j])
         
 
-   
-ani1 = animation.FuncAnimation(fig, animate,interval=m.frequency*1000)
+ani1 = animation.FuncAnimation(fig, animate_cluster,interval=m.frequency*1000)       
+
+
 
 def animate2(i):
-    slow_down_ratio[i]=slow_down[int(i/m.perSlot)]
-    lateness_ratio[i]=lateness[int(i/m.perSlot)]
-    for j in range(M):
-        for t in range(i,m.perSlot*24+i):
-            if t<T:
-                carbonIntensity[j][t]=dframe.iloc[int(t/m.perSlot)][zones[j]]
-       
+    slow_down_ratio[i]=slow_down[i]
+    lateness_ratio[i]=lateness[i] 
     if i>0:
         carbon_metric[i]=carbon_metric[i-1]
         
-    m.get_clusterInfos()
     for j in range(M):
-        utilization[j][i]=(3-j)*0.15#float(m.clusterInfos[0]['gpu'])/float(m.clusterInfos[0]['gpu-cap'])
         carbon_metric[i]=carbon_metric[i]+(utilization[j][i] *carbonIntensity[j][i])
         
     
-    xs=range(0,int(T))
+    xs=range(0,T)
 
     for j in range(3):   
 
         ax2[j].clear()
-        ax2[j].set_xticks(ticks=range(0,int(T),int(2*T/m.Slots)),labels=range(1,m.Slots+1,2),fontsize=5) #every 6 bars add label of slot
+        ax2[j].set_xticks(ticks=range(5,T+1,5),labels=range(5,T+1,5),fontsize=5) #every 6 bars add label of slot
         ax2[j].set_xlabel('Time slots', fontsize = 8)
         if j==0:
             ax2[j].axis([0, T, 0, 10000.1])
             ax2[j].bar(xs, carbon_metric,color ='blue')
-            ax2[j].set_yticks(ticks=range(0,10000,1000),labels=range(0,10000,1000),fontsize=5)
+            ax2[j].set_yticks(ticks=range(1000,10000,1000),labels=range(1000,10000,1000),fontsize=5)
             ax2[j].set_ylabel('Carbon Emission', fontsize = 8)
         if j==1:
             ax2[j].axis([0, T, 0, 3.1])
