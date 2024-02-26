1c6453a7160517272c0923b8de2b79698e8a1ae5
Tayebeh Bahreini <tayebehbahreini@Tayebehs-MacBook-Pro.local>
2024-02-26 11:30:43 -0600

    Update the path of history files

--- code/carbon-intensity-generator.py
+++ code/carbon-intensity-generator.py
@@ -5,7 +5,7 @@ import time
 import subprocess
 kube_path='~/.kube/config'
 period_length=120
-T=75
+T=60
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
 
@@ -13,9 +13,9 @@ def update_carbon():
         for t in range(T):
              
        
-                subprocess.run('cp -f ~/caspian-demo/data/CI/'+str((t%24))+'/CA-ON.csv ~/caspian/monitoring/data/CA-ON.csv ',shell=True)
-                subprocess.run('cp -f ~/caspian-demo/data/CI/'+str((t%24))+'/JP-KN.csv ~/caspian/monitoring/data/JP-KN.csv ',shell=True)
-                subprocess.run('cp -f ~/caspian-demo/data/CI/'+str((t%24))+'/DE.csv ~/caspian/monitoring/data/DE.csv ',shell=True)
+                subprocess.run('cp -f ~/caspian-demo/data/CI/'+str(t)+'/CA-ON.csv ~/caspian/monitoring/data/CA-ON.csv ',shell=True)
+                subprocess.run('cp -f ~/caspian-demo/data/CI/'+str(t)+'/JP-KN.csv ~/caspian/monitoring/data/JP-KN.csv ',shell=True)
+                subprocess.run('cp -f ~/caspian-demo/data/CI/'+str(t)+'/DE.csv ~/caspian/monitoring/data/DE.csv ',shell=True)
                 time.sleep(period_length)
 #------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------
